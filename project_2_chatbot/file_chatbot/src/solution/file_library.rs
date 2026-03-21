c258c77e13cb68f6e8587d17dc762aaaf179faa3
musabsid <musabsid@bu.edu>
2026-03-21 19:17:07 -0400

    load_chat_session_from_file and get_history

--- project_2_chatbot/file_chatbot/src/solution/file_library.rs
+++ project_2_chatbot/file_chatbot/src/solution/file_library.rs
@@ -20,5 +20,21 @@ pub fn save_chat_session_to_file(filename: &str, session: &LlamaChatSession) {
 pub fn load_chat_session_from_file(filename: &str) -> Option<LlamaChatSession> {
     // look at fs::read(...)
     // also look at LlamaChatSession::from_bytes(...)
-    unimplemented!("Loading chat session from file {filename}");
+
+    let result = fs::read(filename);
+    let bytes = match result {
+        Ok(data) => data,
+        Err(bruh) => panic!("Failed to read file {filename}: {bruh}"),
+    };
+    let deserialized_session_result = LlamaChatSession::from_bytes(&bytes);
+
+    let chat_session = match deserialized_session_result {
+        Ok(deserialized_session) => deserialized_session,
+        Err(bruh) => panic!("Failed to deserialize chat session from {filename}: {bruh}"),
+    };
+
+    let out = Some(chat_session);
+    // while it may be more idiomatic to just return this without binding
+    // explicitly defining variables makes type visible; huge for debugging
+    return out;
 }
