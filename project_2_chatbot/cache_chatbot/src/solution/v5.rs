31680b6a47e4771937fc569f9eab013091b5f244
musabsid <musabsid@bu.edu>
2026-03-23 17:34:26 -0400

    v5 chat_with_user

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -1,5 +1,6 @@
 use kalosm::language::*;
 use file_chatbot::solution::file_library;
+use std::fs;
 
 use crate::solution::Cache;
 
@@ -16,23 +17,65 @@ impl ChatbotV5 {
         };
     }
 
+    pub fn load_chat_session_from_file(filename: &str) -> Option<LlamaChatSession> {
+    // look at fs::read(...)
+    // also look at LlamaChatSession::from_bytes(...)
+
+    let result = fs::read(filename);
+    let bytes = match result {
+        Ok(data) => data,
+        Err(bruh) => return None,
+    };
+    let deserialized_session_result = LlamaChatSession::from_bytes(&bytes);
+
+    let chat_session = match deserialized_session_result {
+        Ok(deserialized_session) => deserialized_session,
+        Err(bruh) => panic!("Failed to deserialize chat session from {filename}: {bruh}"),
+    };
+
+    let out = Some(chat_session);
+    // while it may be more idiomatic to just return this without binding
+    // explicitly defining variables makes type visible; huge for debugging
+    return out;
+    }
+
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
         let filename = &format!("{}.txt", username);
         let cached_chat = self.cache.get_chat(&username);
-
-        match cached_chat {
+        
+        // get the Chat<Llama> object
+        let mut active_chat = match cached_chat {
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
-                // The cache does not have the chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
-            }
+                // first, check if a file exists
+                let mut chat_session = 
+                    if let Some(session) = Self::load_chat_session_from_file(filename) {
+                        // if it does, load from it
+                        self.model.chat().with_session(session) 
+                    }
+                    else {
+                        // if it does not, create a new session
+                        self.model.chat().with_system_prompt("The assistant will act like a pirate")
+                    };
+                    chat_session
+                    }
             Some(chat_session) => {
                 println!("chat_with_user: {username} is in the cache! Nice!");
-                // The cache has this chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
-
+                chat_session.clone() 
+                // rust wants a Chat<Llama> object, not a mutable reference. so we clone.
             }
-        }
+        };
+
+        // hopefully, active_chat is the Chat<Llama> object that represents the session. 
+        // now, formulate the response and add it to the session
+        let output = active_chat.add_message(message).await;
+        let result = output.unwrap();
+
+        // next, this must become the most recently used cache conversation
+        self.cache.insert_chat(username, active_chat);
+
+        // return the bot's result
+        return result
     }
 
     pub fn get_history(&mut self, username: String) -> Vec<String> {
