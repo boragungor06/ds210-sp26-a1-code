5a2ac656a322bccaa8aa45dc36b1cfc6fee27840
Bora Gungor <bora@bu.edu>
2026-03-23 18:59:48 -0400

    implemented get_history for v5

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -42,16 +42,29 @@ impl ChatbotV5 {
         match cached_chat {
             None => {
                 println!("get_history: {username} is not in the cache!");
+                if let Some(session) = file_library::load_chat_session_from_file(filename) { // to see if we have the info in the files
+                    let chat = self.model
+                        .chat()
+                        .with_system_prompt("The assistant will act like a pirate")
+                        .with_session(session); // getting back the session from the file
+                    self.cache.insert_chat(username, chat); // putting in the cache it has been used for the next time
+                }
                 // TODO: The cache does not have the chat. What should you do?
                 // Your code goes here.
                 return Vec::new();
             }
             Some(chat_session) => {
                 println!("get_history: {username} is in the cache! Nice!");
+                let session = chat_session.session().unwrap(); //get the current session
+                let history = session.history(); // getting all the emssages
+                let mut output = Vec::new(); // creating an empty output list
+                for message in history.iter().skip(1) {
+                    output.push(message.content().to_string()); // adding all the messages other than the system messag in to the vector
+                }
+                return output;
+
                 // TODO: The cache has this chat. What should you do?
                 // Your code goes here.
-                return Vec::new();
-
             }
         }
     }
