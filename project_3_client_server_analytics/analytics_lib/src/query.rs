d6a87d4faeff2ef76489a022a02365cc0d65cf30
musabsid <musabsid@bu.edu>
2026-04-05 18:23:28 -0400

    fast_rpc client side

--- project_3_client_server_analytics/analytics_lib/src/query.rs
+++ project_3_client_server_analytics/analytics_lib/src/query.rs
@@ -1,5 +1,7 @@
 use crate::dataset::Value;
+use serde::{Deserialize, Serialize};
 
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub enum Condition {
     Equal(String, Value),
     Not(Box<Condition>),
@@ -7,6 +9,7 @@ pub enum Condition {
     Or(Box<Condition>, Box<Condition>), 
 }
 
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub enum Aggregation {
     Count(String),
     Sum(String),
@@ -22,6 +25,7 @@ impl Aggregation {
     }
 }
 
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct Query {
     filter: Condition,
     group_by: String,
--- project_3_client_server_analytics/client/src/solution.rs
+++ project_3_client_server_analytics/client/src/solution.rs
@@ -18,6 +18,6 @@ pub async fn run_slow_rpc(rpc_client: &RPCInterfaceClient, query: Query) -> Data
 pub async fn run_fast_rpc(rpc_client: &RPCInterfaceClient, query: Query) -> Dataset {
     println!("using fast_rpc");
 
-    // You should call fast_rpc here and not slow_rpc.
-    todo!("Implement this");
+    let processed_dataset = rpc_client.fast_rpc(Context::current(), query).await.unwrap();
+    return processed_dataset
 }
