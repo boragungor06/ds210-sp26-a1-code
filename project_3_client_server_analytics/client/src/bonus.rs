a3c7db64fc905ebece51c0f6ac49b796657a0452
musabsid <musabsid@bu.edu>
2026-04-06 19:55:45 -0400

    comments

--- project_3_client_server_analytics/client/src/bonus.rs
+++ project_3_client_server_analytics/client/src/bonus.rs
@@ -37,7 +37,8 @@ fn parse_query_from_string(input: String) -> Query {
 
     // the vec containing only the condition tokens
     let condition_as_vec = words.clone()[start_idx..end_idx].to_vec();
-    let final_filter: Condition;
+    
+    let final_filter: Condition; // this will hold the filter condition
 
     // now, we want to divide this substring into smaller instructions, divided by AND or OR
     // so that each instruction within each OR/AND clause is seperate
@@ -63,6 +64,9 @@ fn parse_query_from_string(input: String) -> Query {
     }
     // now, we can start parsing the condition. this code is long.
     // but, it simply repeats the same logic 3 times: for left and for right, or for only the original argument
+    // in this segment, we accomplish: (1) cleaning off punctuation from the query, 
+    // (2) feeding the resulting strings directly into a Condition object
+    // (3) handling the Condition for there was an AND, an OR, or neither
 
     // for LEFT and RIGHT
     if operator == "AND" || operator == "OR" {
@@ -71,21 +75,25 @@ fn parse_query_from_string(input: String) -> Query {
         let mut left_last = left[left.len() - 1];
         let mut left_is_not = false;
     
+        // remove punctuation from the front of the first word
         while left_first.starts_with('(') || left_first.starts_with('!') || left_first.starts_with('"') {
             if left_first.starts_with('!') { 
                 left_is_not = true; 
             }
-            left_first = &left_first[1..]; // move up one char
+            left_first = &left_first[1..]; // move up one char to remove the punctuation
         }
 
+        // remove punctuation from the end of the last word
         while left_last.ends_with(')') || left_last.ends_with('"') {
                 left_last = &left_last[..left_last.len()-1];
         }
 
+        // remove punctuation from the front of the last word
         while left_last.starts_with('"') {
             left_last = &left_last[1..];
         }
 
+        // creating the Condition
         let mut left_condition = Condition::Equal(
             left_first.to_string(), 
             Value::String(left_last.to_string())
@@ -100,6 +108,7 @@ fn parse_query_from_string(input: String) -> Query {
         let mut right_last = right[right.len() - 1];
         let mut right_is_not = false;
     
+        // remove punctuation from the front of the first word
         while right_first.starts_with('(') || right_first.starts_with('!') || right_first.starts_with('"') {
             if right_first.starts_with('!') { 
                 right_is_not = true; 
@@ -107,18 +116,21 @@ fn parse_query_from_string(input: String) -> Query {
             right_first = &right_first[1..];
         }
 
+        // remove punctuation from the end of the last word
         while right_last.ends_with(')') || right_last.ends_with('"') {
             if !right_last.is_empty() {
                 right_last = &right_last[..right_last.len() - 1];
             } else { break; }
         }
 
+        // remove punctuation from the front of the last word
         while right_last.starts_with('"') {
             if !right_last.is_empty() {
                 right_last = &right_last[1..];
             } else { break; }
         }
 
+        // creating the Condition
         let mut right_condition = Condition::Equal(
             right_first.to_string(), 
             Value::String(right_last.to_string())
@@ -141,23 +153,27 @@ fn parse_query_from_string(input: String) -> Query {
         let mut last = condition_as_vec[condition_as_vec.len() - 1];
         let mut is_not = false;
     
+        // remove punctuation from the front of the first word
         while first.starts_with('(') || first.starts_with('!') || first.starts_with('"') {
             if first.is_empty() { break; }
             if first.starts_with('!') { is_not = true; }
             first = &first[1..];
         }
 
+        // remove punctuation from the end of the last word
         while last.ends_with(')') || last.ends_with('"') {
             if last.is_empty() { break; }
             last = &last[..last.len() - 1];
         }
 
+        // remove punctuation from the front of the last word
         while last.starts_with('"') {
             if last.is_empty() { break; }
             last = &last[1..];
         }
 
-       let mut base = Condition::Equal(
+        // creating the Condition
+        let mut base = Condition::Equal(
             first.to_string(), 
             Value::String(last.to_string())
         );
@@ -170,7 +186,6 @@ fn parse_query_from_string(input: String) -> Query {
     }
 
     // AGGREGATION:
-
     let operation_category = words[words.len()-1].to_string();
     let operation_type = words[words.len()-2];
     let mut operation = Aggregation::Count(String::from("dummy"));
@@ -192,6 +207,7 @@ fn parse_query_from_string(input: String) -> Query {
     let word_after_groupby = words[location_of_group + 2].to_string();
     // this code gets the word after group by => "GROUP by class", this gets the "class"
     
+    // now that we have the condition, the group_by, and the aggregation method, simply combine them into final Query
     let x = Query::new(final_filter, word_after_groupby, operation);
     return x
     }
